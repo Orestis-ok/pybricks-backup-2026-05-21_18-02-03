58bda027bd39f55cfacd16298597fbae611320a2
Orestis K <okerkines@gmail.com>
2026-05-21 22:59:08 +0300

    Update Route2.py

--- Route2.py
+++ Route2.py
@@ -12,13 +12,6 @@ from MyLibrary import *
 def Route2(): 
 
     PortView_Battery()
-    # MoveStraight_Distance (300,300,450,True,True,Stop.BRAKE)
-    # PointTurn_Angle (300, 300, 90, True, Stop.BRAKE)
-    # MoveSteering_Seconds(300, 20, 300)
-    # leftArm.run_time(300, 300, then=Stop.BRAKE, wait=True)
-
-    MoveSteering_Seconds(600, 20, 1000)
-    MoveSteering_Seconds(600, 10, 450)
-
+    
     
 
