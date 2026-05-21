cbc3dfb90208f6b4bd2201f7bd18a807445f44f6
Orestis K <okerkines@gmail.com>
2026-05-21 22:54:48 +0300

    Create Route1.py

---
+++ Route1.py
@@ -0,0 +1,14 @@
+######################## Pyricks library ########################
+
+from pybricks.parameters import Color, Direction, Stop, Icon
+from pybricks.tools import wait, StopWatch, Matrix
+
+######################## Custom program ########################
+
+from MyLibrary import *
+
+  
+####################### Route program ########################
+def Route1(): 
+    
+    PortView_Battery()
