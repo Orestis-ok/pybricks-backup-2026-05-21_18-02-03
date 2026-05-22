9dda69916ad6c2f73e7ea34c32b10234031c7f64
Orestis K <okerkines@gmail.com>
2026-05-22 14:56:48 +0300

    Update Route2.py

--- Route2.py
+++ Route2.py
@@ -1,4 +1,4 @@
-######################## Pyricks library ########################
+####################### Pyricks library ########################
 
 from pybricks.parameters import Color, Direction, Stop, Icon
 from pybricks.tools import wait, StopWatch, Matrix
@@ -7,27 +7,21 @@ from pybricks.tools import wait, StopWatch, Matrix
 
 from MyLibrary import *
 
-  
-####################### Route program ########################
-def Route2(): 
 
+####################### Route program ########################
+def Route3(): 
+	
     PortView_Battery()
     
-    ### Πορέια προς βόρειο τοίχο μπορστά από χάρτη ###
+    ### Πορέια προς χάρτη & βούρτσα ###
     MoveSteering_Seconds(-50, 0, 300)
+	
+	MoveStraight_Distance (600,400,725,True,True,Stop.BRAKE)
     
-	MoveStraight_Distance (600,600,800,True,True,Stop.BRAKE)
-    PointTurn_Angle (300, 300, 180, True, Stop.BRAKE)
-    MoveStraight_Distance (600,600,-300,True,True,Stop.BRAKE)
-    MoveStraight_Distance (600,600,60,True,True,Stop.BRAKE)
-    PointTurn_Angle (500, 400, -105, True, Stop.BRAKE)
+    ### Χάρτης ###
+    PointTurn_Angle (300, 300, -45, True, Stop.BRAKE)
+    MoveStraight_Distance (450,300,150,True,True,Stop.BRAKE)
+    rightArm.run_time(400, 700, then=Stop.BRAKE, wait=True)
+    MoveStraight_Distance (300,300,-200,True,True,Stop.BRAKE)
     
-    ### Προσεκτική & τρενάκι ###
-    MoveStraight_Distance (400,300,100,True,True,Stop.BRAKE)
-    leftArm.run_time(-800,1000, then=Stop.BRAKE, wait=False)
-    rightArm.run_time(200, 2000, then=Stop.BRAKE, wait=True)
-    
-	### Επιστροφή στην βάση ###
-    MoveStraight_Distance (250,200,-100,True,True,Stop.BRAKE)
-    PointTurn_Angle (300, 300, 100, True, Stop.BRAKE)
-	MoveStraight_Distance (800,600,770,True,True,Stop.BRAKE)
+    ### Βούρτσα ###
