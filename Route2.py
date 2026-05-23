44abb3b11d9ef334c68f29e6042862d1d3decede
Orestis K <okerkines@gmail.com>
2026-05-23 22:58:11 +0300

    Update Route2.py

--- Route2.py
+++ Route2.py
@@ -23,5 +23,3 @@ def Route3():
     MoveStraight_Distance (450,300,150,True,True,Stop.BRAKE)
     rightArm.run_time(400, 700, then=Stop.BRAKE, wait=True)
     MoveStraight_Distance (300,300,-200,True,True,Stop.BRAKE)
-    
-    ### Βούρτσα ###
